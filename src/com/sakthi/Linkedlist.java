51bd0ce5901a58c927d0a580270f12d70dc64b6d
codewithsakthi <developersakthi23@gmail.com>
2024-03-06 13:19:41 +0530

    First Commit

---
+++ src/com/sakthi/Linkedlist.java
@@ -0,0 +1,8 @@
+package com.sakthi;
+
+public class Linkedlist {
+    public class Node{
+        private int value;
+        private Node next;
+    }
+}
---
+++ src/com/sakthi/Main.java
@@ -0,0 +1,7 @@
+package com.sakthi;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
