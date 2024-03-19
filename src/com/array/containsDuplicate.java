bc86d1fa61e57e049188139c80db526fd93a64fa
codewithsakthi <developersakthi23@gmail.com>
2024-03-19 10:28:24 +0530

    Daily Learn

--- src/com/array/containsDuplicate.java
+++ src/com/array/containsDuplicate.java
@@ -5,14 +5,6 @@ import java.util.Map;
 
 public class containsDuplicate {
     public static void main(String[] args) {
-        int[] a ={1,2,3,1};
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int j : a) {
-            map.merge(j, 1, Integer::sum);
-        }
-
-            System.out.println("false");
-        }
 
     }
 }
